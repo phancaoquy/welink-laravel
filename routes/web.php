dce72eabe91320f805e69f5661a83c680260522c
ngothanhbinh <ntbinh14122004@gmail.com>
2025-01-03 19:05:45 +0700

    create branch test

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route mặc định của Laravel
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Route mới để hiển thị "Hello World"
+Route::get('/greeting', function () {
+    return 'Hello World';
 });
